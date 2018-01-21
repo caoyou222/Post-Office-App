9705499769cd168c8f9d31cfb0200794d538d0cc
cao1 <cao1@stolaf.edu>
2018-01-21 16:55:37 -0600

    android app: complete package object and checking interfaces

--- app/app/src/main/java/com/example/youcao/app/MainActivity.java
+++ app/app/src/main/java/com/example/youcao/app/MainActivity.java
@@ -1,13 +1,10 @@
 package com.example.youcao.app;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,17 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        PackageAdapter teacherAdapter = new PackageAdapter(this, R.layout.package_item, Package.getAllPackages());
+
+        ListView listView = (ListView) findViewById(R.id.package_listView);
+
+        listView.setAdapter(teacherAdapter);
     }
 
     @Override
---
+++ app/app/src/main/java/com/example/youcao/app/Package.java
@@ -0,0 +1,95 @@
+package com.example.youcao.app;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.*;
+import java.util.Calendar;
+/**
+ * Created by youcao on 1/20/18.
+ */
+
+
+    public class Package {
+
+        private int year;
+        private int month;
+        private int day;
+        private String trackingNo;
+        private String last;
+        private String first;
+        private String carrier;
+        private int signed = 0;
+
+        public Package(String trackingNo, String last, String first, String carrier) {
+            this.trackingNo = trackingNo;
+            this.last = last;
+            this.first = first;
+            this.carrier = carrier;
+            Date date = new Date(); // your date
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(date);
+            this.year = cal.get(Calendar.YEAR);
+            this.month = cal.get(Calendar.MONTH)+1;
+            this.day = cal.get(Calendar.DAY_OF_MONTH);
+
+        }
+
+        public String getTrackingNo() {
+            return trackingNo;
+        }
+
+        public String getCarrier(){
+            return carrier;
+        }
+
+        public void setSigned(){
+            this.signed = 1;
+        }
+
+        public String getSigned(){
+            if(this.signed == 1) return "Signed";
+            else return "Unsigned";
+        }
+
+        public void setTrackingNo(String trackingNo) {
+            this.trackingNo = trackingNo;
+        }
+
+        public String getLast() {
+            return last;
+        }
+
+        public void setLast(String last) {
+            this.last = last;
+        }
+
+        public String getFirst() {
+            return first;
+        }
+
+        public void setFirst(String first) {
+            this.first = first;
+        }
+
+        public int getYear(){
+            return year;
+        }
+
+        public int getMonth(){
+            return month;
+        }
+
+        public int getDay(){
+            return day;
+        }
+
+
+        public static List<Package> getAllPackages() {
+            List<Package> packages = new ArrayList<Package>();
+
+            packages.add(new Package("00000000", "Cao", "You","DHL"));
+            packages.add(new Package("11111111", "Ge", "Muchen","UPS"));
+
+
+            return packages;
+        }
+    }
