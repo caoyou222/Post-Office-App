c23f3c461b0b035a82e24c6c61e5f9cb97e69763
cao1 <cao1@stolaf.edu>
2018-01-24 14:33:58 -0600

    fixed bugs

--- PO_proj/screens/student.js
+++ PO_proj/screens/student.js
@@ -64,7 +64,7 @@ export default class student extends Component {
             <View style={styles.sectionHeader}>
                 <Text>{pack.trackno}</Text>
             </View>
-            //</TouchableOpacity>
+            </TouchableOpacity>
         );
         }
         
