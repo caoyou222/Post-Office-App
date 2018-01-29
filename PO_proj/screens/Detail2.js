55c64523ed8853a27167d68984f752a42a9a368b
cao1 <cao1@stolaf.edu>
2018-01-29 01:15:54 -0600

    added model for second detail page

--- PO_proj/screens/Detail2.js
+++ PO_proj/screens/Detail2.js
@@ -3,6 +3,7 @@ import {Image, Text, StyleSheet, Button, View, Dimensions, Vibration, TouchableO
 import {StackNavigator} from 'react-navigation'
 import {Card, ListItem} from 'react-native-elements'
 
+const SERVER_PACKAGES = 'http://rns202-5.cs.stolaf.edu:28425/sign'
 export default class Detail2 extends React.Component {
  constructor(props){
  super(props);
@@ -17,6 +18,28 @@ export default class Detail2 extends React.Component {
     headerBackTitleStyle: {color: 'white'},
     headerTintColor: 'white',
  }
+
+  _update(){
+    const { params } = this.props.navigation.state;
+    return fetch(SERVER_PACKAGES, {
+    method: "PATCH", body:`trackno=${params.trackno}` ,
+    headers: {"Content-type": "application/x-www-form-urlencoded; charset=UTF-8"} })
+    .then((res) => {
+      if (res.ok) {
+        console.log("it worked!");
+      } else {
+        console.log("nope")
+      }
+    })
+
+  }
+
+  _mark(){
+    this._update();
+    //console.log(keywords);
+    //this._onRefresh();
+  }
+
  render(){
  const { navigate } = this.props.navigation;
  const { params } = this.props.navigation.state;
@@ -26,7 +49,7 @@ export default class Detail2 extends React.Component {
    <Text style={{height: 20, fontWeight: 'bold',color: '#514e48'}}>Carrier                           {params.carrier}</Text>
    <Text style={{height: 20, fontWeight: 'bold',color: '#514e48'}}>Status:                           {params.status}</Text>
    <Text style={{height: 20, fontWeight: 'bold',color: '#514e48'}}>Date:                              {params.month}/{params.day}/{params.year}</Text>
-   <TouchableOpacity style={styles.buttonContainer}>
+   <TouchableOpacity style={styles.buttonContainer} onPress = {this._mark.bind(this)}>
         <Text style={{color: 'grey', marginLeft: 6, fontSize: 15, fontWeight: 'bold'}}> Mark as Signed </Text>
    </TouchableOpacity>
   </Card>
